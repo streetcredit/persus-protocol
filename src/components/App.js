c06b7f4db50e3c717878afdf641050763b1f1f4a
Street Credit <43270978+streetcredit@users.noreply.github.com>
2022-02-23 19:03:03 -0500

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -115,7 +115,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="http://street.credit"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
